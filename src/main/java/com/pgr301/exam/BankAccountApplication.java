bc8220b739091d588ff0dba7523faca7a5e75264
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2023-12-20 10:20:58 +0100

    Adding more code but from a new branch

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Adding comment to test Github action workflow
+//Adding more code but from a new branch
 @SpringBootApplication
 public class BankAccountApplication {
     public static void main(String[] args) {

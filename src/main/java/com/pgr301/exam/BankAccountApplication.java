ab270fb68a9e605817da91631414355cae202309
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2023-12-20 10:10:44 +0100

    Adding comment to test Github action workflow

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -3,6 +3,7 @@ package com.pgr301.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Adding comment to test Github action workflow
 @SpringBootApplication
 public class BankAccountApplication {
     public static void main(String[] args) {

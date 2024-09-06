5e5e568b26f6aab1844902fbf875a2ac4300366e
EC2 Default User <ec2-user@ip-172-31-35-44.eu-west-1.compute.internal>
2024-09-06 09:47:20 +0000

    Added a private variable5

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
-        private String boom;
+        String boom;
     }
 }
